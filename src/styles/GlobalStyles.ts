75601afc0c9c63099addf44d2f3821ac83efd426
Daniel Salinas Merino <danielsalinas305@gmail.com>
2022-03-01 11:20:30 +0100

    feat(GlobalStyles): Added set of color variables

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -8,6 +8,23 @@ const GlobalStyles = createGlobalStyle`
     body {
         font-family: "Times New Roman", Times, serif;
     }
+
+    /*
+        Color variables
+    */
+    :root {
+        --black-100: #000000;
+        --black-090: #191919;
+        --black-080: #323232;
+        --black-070: #4c4c4c;
+        --black-060: #666666;
+        --black-050: #7f7f7f;
+        --black-040: #999999;
+        --black-030: #b2b2b2;
+        --black-020: #cccccc;
+        --black-010: #e5e5e5;
+        --black-000: #ffffff;
+    }
 `;
 
 export default GlobalStyles;
