99506a32e178374c036e88fd08ef20b69bab6f7f
Daniel Salinas Merino <danielsalinas305@gmail.com>
2022-03-01 11:33:48 +0100

    feat: Ongoing logic to apply media queries on default template

--- src/components/templates/DefaultTemplate.tsx
+++ src/components/templates/DefaultTemplate.tsx
@@ -33,6 +33,13 @@ const Wrapper = styled.div`
   width: 100%;
   min-height: 100vh;
   padding: 10vh 10vw 10vh 10vw;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+  @media (min-width: 769px) and (max-width: 1024px) {
+    flex-direction: column;
+    align-items: center;
+  }
 `;
 
 const LeftContentWrapper = styled.div`
@@ -40,6 +47,14 @@ const LeftContentWrapper = styled.div`
   align-items: center;
   justify-content: center;
   width: 35%;
+  @media (max-width: 768px) {
+    width: 100%;
+    background-color: red;
+  }
+  @media (min-width: 769px) and (max-width: 1024px) {
+    width: 100%;
+    background-color: red;
+  }
 `;
 
 const RightContentWrapper = styled.div`
@@ -47,4 +62,12 @@ const RightContentWrapper = styled.div`
   align-items: center;
   justify-content: center;
   width: 60%;
+  @media (max-width: 768px) {
+    width: 100%;
+  }
 `;
+
+/* 320px — 480px: Mobile devices
+481px — 768px: iPads, Tablets
+769px — 1024px: Small screens, laptops
+1025px — 1200px: Desktops, large screens */
