04ab4fd1fecd81333bc8652df55a69396340bbf7
danielsalinasmerino <danielsalinas305@gmail.com>
2022-03-05 18:06:15 +0100

    feat: Added logo

--- src/Element.tsx
+++ src/Element.tsx
@@ -1,16 +1,10 @@
 import React from "react";
 
+import logo from "./assets/images/logo.png";
+
 const Element = () => {
   return (
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tincidunt
-      ligula non felis rutrum, sit amet mollis ipsum ullamcorper. Etiam eu diam
-      sit amet mi vestibulum semper. Praesent imperdiet est quis augue blandit
-      ultricies. Sed ornare nisl condimentum, dictum quam eu, dictum enim. Cras
-      leo magna, pharetra cursus accumsan nec, posuere vitae odio. Praesent ac
-      tellus a magna feugiat ullamcorper. Sed tincidunt imperdiet molestie.
-      Donec elit sem, dignissim a fermentum molestie, hendrerit vitae neque.
-    </p>
+    <img src={logo} alt="Logo" />
   );
 };
 
