2176cdd805bcb26743519bc4520f9370d81294d8
Daniel Salinas Merino <danielsalinas305@gmail.com>
2022-03-23 12:58:22 +0100

    feat(DefaultTemplate): Improved DefaultTemplate

--- src/components/templates/DefaultTemplate/DefaultTemplate.tsx
+++ src/components/templates/DefaultTemplate/DefaultTemplate.tsx
@@ -31,12 +31,21 @@ const Wrapper = styled.div`
   width: 100%;
   min-height: 100vh;
   padding: 10vh 10vw 10vh 10vw;
-  @media (max-width: 768px) {
+  @media (min-width: 320px) and (max-width: 480px) {
+    align-items: center;
+    flex-direction: column;
+  }
+  @media (min-width: 481px) and (max-width: 768px) {
+    align-items: center;
     flex-direction: column;
   }
   @media (min-width: 769px) and (max-width: 1024px) {
-    flex-direction: column;
     align-items: center;
+    flex-direction: column;
+  }
+  @media (min-width: 1025px) and (max-width: 1200px) {
+    align-items: center;
+    flex-direction: column;
   }
 `;
 
@@ -45,13 +54,17 @@ const LeftContentWrapper = styled.div`
   align-items: center;
   justify-content: center;
   width: 35%;
-  @media (max-width: 768px) {
+  @media (min-width: 320px) and (max-width: 480px) {
     width: 100%;
-    background-color: red;
+  }
+  @media (min-width: 481px) and (max-width: 768px) {
+    width: 100%;
   }
   @media (min-width: 769px) and (max-width: 1024px) {
     width: 100%;
-    background-color: red;
+  }
+  @media (min-width: 1025px) and (max-width: 1200px) {
+    width: 100%;
   }
 `;
 
@@ -60,14 +73,18 @@ const RightContentWrapper = styled.div`
   align-items: center;
   justify-content: center;
   width: 60%;
-  @media (max-width: 768px) {
+  @media (min-width: 320px) and (max-width: 480px) {
+    width: 100%;
+  }
+  @media (min-width: 481px) and (max-width: 768px) {
+    width: 100%;
+  }
+  @media (min-width: 769px) and (max-width: 1024px) {
+    width: 100%;
+  }
+  @media (min-width: 1025px) and (max-width: 1200px) {
     width: 100%;
   }
 `;
 
 export default DefaultTemplate;
-
-/* 320px — 480px: Mobile devices
-481px — 768px: iPads, Tablets
-769px — 1024px: Small screens, laptops
-1025px — 1200px: Desktops, large screens */
